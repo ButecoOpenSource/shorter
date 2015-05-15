e2459b42f433a9323cf307c3f59f438211e59310
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2015-05-15 01:04:33 -0300

    Command line argument port.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python main.py
+web: python python app.py --port=$PORT
--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urls
 import tornado.ioloop
 import tornado.web
+import urls
 
 from settings import settings
+from tornado.options import define, options
 
+define("port", default="5000", help="Port")
 
 application = tornado.web.Application(urls.url_patterns, **settings)
 
 if __name__ == '__main__':
-    application.listen(8888)
+    tornado.options.parse_command_line()
+    application.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
