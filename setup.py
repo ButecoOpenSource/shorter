6835656745758a6ee438b0f24948471f4e266ed7
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2016-02-19 01:35:40 -0200

    Fix for dist.

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='BitlyShorter',
-    version='1.1.1',
+    version='1.1.2',
     author='Alexandre Vicenzi',
     author_email='vicenzi.alexandre@gmail.com',
     maintainer='Alexandre Vicenzi',
     maintainer_email='vicenzi.alexandre@gmail.com',
     packages=['shorter'],
     install_requires=['PyYAML==3.11', 'requests==2.9.1', 'tornado==4.3'],
+    include_package_data=True,
     url='https://github.com/alexandrevicenzi/shorter',
     bugtrack_url='https://github.com/alexandrevicenzi/shorter/issues',
     license='MIT',
@@ -24,12 +22,18 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'License :: OSI Approved :: MIT License',
         'Operating System :: MacOS',
         'Operating System :: Microsoft',
         'Operating System :: POSIX',
         'Operating System :: Unix',
         'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
         'Topic :: Utilities',
     ],
 )
